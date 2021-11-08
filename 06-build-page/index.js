2d39d7c608ff6a485a9f6048a49b14531dfa5b9c
Arxons <vasyrin1312@gmail.com>
2021-11-09 01:13:07 +0300

    feat: done subtask 06

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -28,51 +28,63 @@ fs.readFile(path.join(__dirname, 'project-dist', 'style.css'), 'utf-8', (err, da
 
 })
 
-
-fs.readFile(path.join(__dirname, 'template.html'), 'utf-8', (err, data) => {
-    if (err) throw err;
-    fs.appendFile(path.join(projectDist, 'index.html'), data, () => { })
-})
 fs.readdir(path.join(__dirname, 'components'), (err, cont) => {
     if (err) throw err;
     let res = [];
-    cont.forEach(item => {
-        const ext = path.parse(item).ext
-        if (ext == '.html') {
-            fs.readFile(path.join(__dirname, 'components', item), 'utf-8', (err, data) => {
-                if (err) throw err;
-                const htmlToReplace = data;
-                fs.readFile(path.join(__dirname, 'template.html'), 'utf-8', (err, data) => {
-                    const fileName = path.parse(item).name
-                    const regexp = /{{(.*)}}/g
-
-                    data = data.split(regexp)
-                    for (let i = 0; i < data.length; i++) {
-                        if (data[i] == fileName) {
-                            data[i] = htmlToReplace
-                        }
-                    }
-                    data = data.join('')
-                    fs.appendFile(path.join(projectDist, 'index.html'), data, () => { })
-
-                    //     if (fileName == 'header') {
-                    //         data = data.replace('{{header}}', htmlToReplace)
-                    //     } else if (fileName == 'footer') {
-                    //         data = data.replace('{{footer}}', htmlToReplace)
-                    //     } else if (fileName == 'articles') data = data.replace('{{articles}}', htmlToReplace)
-                    // })
-
-                });
-            }
-            )
-        }
-    })
-    setTimeout(() => {
+    fs.readFile(path.join(__dirname, 'template.html'), 'utf-8', (err, data) => {
+        if (err) throw err;
+        fs.writeFile(path.join(projectDist, 'index.html'), data, () => { })
         const regexp = /{{(.*)}}/g
-        res = res
-            .join('')
+        data = data
             .split(regexp)
-        const jopa = new Set(res)
-        console.log(jopa)
-    }, 100)
+            .forEach(item => {
+                res.push(item)
+            })
+    });
+    setTimeout(() => {
+        cont.forEach(item => {
+            const ext = path.parse(item).ext
+            const fileName = path.parse(item).name
+            if (ext == '.html') {
+                fs.readFile(path.join(__dirname, 'components', item), 'utf-8', (err, data) => {
+                    if (err) throw err;
+                    const htmlToReplace = data;
+                    res.forEach((curitem, index) => {
+                        if (curitem == fileName) {
+                            res.splice(index, 1, htmlToReplace)
+                            fs.writeFile(path.join(projectDist, 'index.html'), res.join(''), () => { })
+
+                        }
+                    })
+                })
+            }
+        })
+
+    }, 100);
 })
+
+
+//Внимание, копирование ассетов работает только с 1 уровнем вложенности, не успел доделать рекурсию
+fs.mkdir(path.join(__dirname, 'project-dist', 'assets'), () => { })
+fs.readdir(path.join(__dirname, 'assets'), (err, cont) => {
+    if (err) throw err
+    cont.forEach(item => {
+        fs.stat(path.join(__dirname, 'assets', item), (err, stats) => {
+            if (stats.isFile()) {
+                fs.copyFile(path.join(__dirname, 'assets', item), path.join(__dirname, 'project-dist', 'assets', item), () => { })
+            } else {
+                fs.mkdir(path.join(__dirname, 'project-dist', 'assets', item), () => { })
+                fs.readdir(path.join(__dirname, 'assets', item), (err, c) => {
+                    if (err) throw err
+                    c.forEach(i => {
+
+                        fs.copyFile(path.join(__dirname, 'assets', item, i), path.join(__dirname, 'project-dist', 'assets', item, i), () => { })
+                    })
+                })
+            }
+
+        })
+    })
+})
+//Если вдруг у вас какой то модуль не работает, пожалуйста напишите в ЛС перед оцениванием.
+//тестировал по-разному, но возможно мог упустить что то.
